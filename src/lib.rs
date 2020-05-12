8c3c985c262873ed16d99fb050e1336266e4cea5
iwahbe <ian@wahbe.com>
2020-05-11 17:06:11 -0700

    Switched to proc_macro2 for internal use

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 #![feature(proc_macro_diagnostic)]
 extern crate proc_macro;
 use proc_macro::TokenStream;
+use proc_macro2;
 use quote::quote;
 use syn;
 use syn::spanned::Spanned;
@@ -47,8 +48,7 @@ pub fn part_app(attr: TokenStream, item: TokenStream) -> TokenStream {
             let added_unit = concat_ident(name, "Added");
             let empty_unit = concat_ident(name, "Empty");
             let argument_vector = argument_vector(&func.sig.inputs);
-            println!("predicate: {}", predicate);
-            let body: &Box<syn::Block> = &func.block;
+
             let func_struct = main_struct(&predicate, &argument_vector, &func.sig.output);
             let generator_func = generator_func(
                 &predicate,
@@ -56,16 +56,29 @@ pub fn part_app(attr: TokenStream, item: TokenStream) -> TokenStream {
                 &argument_vector,
                 &func.sig.output,
                 &empty_unit,
-                body,
+                &func.block,
             );
-            println!("struct: {}", func_struct);
-            println!("generator: {}", generator_func);
-            quote! {
+            let unit_structs = quote! {
                 #[allow(non_camel_case_types,non_snake_case)]
                 struct #added_unit;
                 #[allow(non_camel_case_types,non_snake_case)]
                 struct #empty_unit;
-            }
+            };
+
+            println!("generator: {}", generator_func);
+
+            // assemble output
+            let mut out = proc_macro2::TokenStream::new();
+            out.extend(unit_structs);
+            out.extend(func_struct);
+            out.extend(generator_func);
+            out.extend(final_call(
+                &predicate,
+                &argument_vector,
+                &func.sig.output,
+                &added_unit,
+            ));
+            TokenStream::from(out)
         }
         _ => {
             func_item
@@ -75,12 +88,47 @@ pub fn part_app(attr: TokenStream, item: TokenStream) -> TokenStream {
                     "Only functions can be partially applied, for structs use the builder pattern",
                 )
                 .emit();
-            quote! { #func_item }
+            let out: proc_macro::TokenStream = quote! { #func_item }.into();
+            out
         }
     }
-    .into()
 }
 
+/// The portion of the signature necessary for each impl block
+fn impl_signature<'a>(
+    args: &Vec<&syn::PatType>,
+    ret_type: &'a syn::ReturnType,
+) -> proc_macro2::TokenStream {
+    let arg_names = arg_names(&args);
+    let arg_types = arg_types(&args);
+    let augmented_names = augmented_argument_names(&arg_names);
+
+    let out = quote! {
+        #(#augmented_names: FnOnce() -> #arg_types,)*
+        BODYFN: FnOnce(#(#arg_types)*) #ret_type
+    };
+    out.into()
+}
+
+fn final_call<'a>(
+    struct_name: &syn::Ident,
+    args: &Vec<&syn::PatType>,
+    ret_type: &'a syn::ReturnType,
+    unit_added: &'a syn::Ident,
+) -> proc_macro2::TokenStream {
+    let impl_sig = impl_signature(args, ret_type);
+    let arg_names = arg_names(args);
+    let aug_args = augmented_argument_names(&arg_names);
+    quote! {
+        impl <#impl_sig>
+            #struct_name<#(#unit_added, #aug_args,)*, BODYFN>
+        {
+
+        }
+    }
+}
+
+/// The function called by the user to create an instance of a partially applicable function
 fn generator_func<'a>(
     struct_name: &'a syn::Ident,
     name: &'a syn::Ident,
@@ -88,12 +136,13 @@ fn generator_func<'a>(
     ret_type: &'a syn::ReturnType,
     empty_unit: &'a syn::Ident,
     body: &'a Box<syn::Block>,
-) -> proc_macro::TokenStream {
+) -> proc_macro2::TokenStream {
     let arg_names = arg_names(&args);
     let arg_types = arg_types(&args);
     let marker_names = marker_names(&arg_names);
 
-    let out = quote! {
+    quote! {
+        #[allow(non_camel_case_types,non_snake_case)]
         fn #name< #(#arg_names,)* >() -> #struct_name<#(#empty_unit,#arg_names,)*
         impl FnOnce(#(#arg_types,)*) #ret_type>
         where
@@ -106,10 +155,10 @@ fn generator_func<'a>(
             }
         }
 
-    };
-    out.into()
+    }
 }
 
+/// A vector of all argument names
 fn arg_names<'a>(args: &Vec<&syn::PatType>) -> Vec<syn::Ident> {
     args.iter()
         .map(|f| {
@@ -119,16 +168,20 @@ fn arg_names<'a>(args: &Vec<&syn::PatType>) -> Vec<syn::Ident> {
         .collect()
 }
 
+/// returns the vector of names used to hold PhantomData
 fn marker_names(names: &Vec<syn::Ident>) -> Vec<syn::Ident> {
     names.iter().map(|f| concat_ident(f, "m")).collect()
 }
 
+/// Concatenates a identity with a string, returning a new identity with the same span.
 fn concat_ident<'a>(ident: &'a syn::Ident, end: &str) -> syn::Ident {
     let name = format!("{}___{}", quote! {#ident}, end);
     syn::Ident::new(&name, ident.span())
 }
 
+/// Gets the name a of function.
 fn get_name<'a>(func: &'a syn::ItemFn) -> &'a syn::Ident {
+    // TODO: move this check somewhere else
     if let Some(r) = &func.sig.receiver() {
         r.span()
             .unstable()
@@ -138,6 +191,7 @@ fn get_name<'a>(func: &'a syn::ItemFn) -> &'a syn::Ident {
     &func.sig.ident
 }
 
+/// Filter an argument list to a pattern type
 fn argument_vector<'a>(
     args: &'a syn::punctuated::Punctuated<syn::FnArg, syn::token::Comma>,
 ) -> Vec<&syn::PatType> {
@@ -149,6 +203,7 @@ fn argument_vector<'a>(
         .collect()
 }
 
+/// Retrieves the identities of an the argument list
 fn arg_types<'a>(args: &Vec<&syn::PatType>) -> Vec<syn::Ident> {
     args.iter()
         .map(|f| {
@@ -158,20 +213,26 @@ fn arg_types<'a>(args: &Vec<&syn::PatType>) -> Vec<syn::Ident> {
         .collect()
 }
 
+fn augmented_argument_names<'a>(arg_names: &Vec<syn::Ident>) -> Vec<syn::Ident> {
+    arg_names.iter().map(|f| concat_ident(f, "FN")).collect()
+}
+
+/// Generates the main struct for the partially applicable function.
+/// All other functions are methods on this struct.
 fn main_struct<'a>(
     name: &'a syn::Ident,
     args: &Vec<&syn::PatType>,
     ret_type: &'a syn::ReturnType,
-) -> proc_macro::TokenStream {
+) -> proc_macro2::TokenStream {
     let arg_types = arg_types(&args);
 
     let arg_names = arg_names(&args);
 
-    let arg_augmented: Vec<_> = arg_names.iter().map(|f| concat_ident(f, "FN")).collect();
+    let arg_augmented = augmented_argument_names(&arg_names);
 
     let names_with_m = marker_names(&arg_names);
 
-    TokenStream::from(quote!(
+    quote!(
         #[allow(non_camel_case_types,non_snake_case)]
         struct #name <#(#arg_names, #arg_augmented,)*BODYFN>
         where
@@ -182,5 +243,5 @@ fn main_struct<'a>(
             #(#arg_names: Option<#arg_augmented>,)*
             body: BODYFN,
         }
-    ))
+    )
 }
